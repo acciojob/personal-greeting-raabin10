e30db5f6e3200e43373dda4f94bb3321fa258919
RAABIN SINGH  S A <112077212+raabin10@users.noreply.github.com>
2023-12-14 06:57:19 +0000

    final

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,17 @@
 
-import React from "react";
+import React ,{useState}from "react";
 import './../styles/App.css';
 
 const App = () => {
+  let [text, setText]  = useState('');
   return (
     <div>
-        {/* Do not remove the main div */}
+      <p>Enter your name:</p>
+      <input
+       onChange={(e)=>setText(e.target.value)}
+       value={text}/>
+       {text.length > 0 ? <p>Hello {text}!</p>: null }
     </div>
   )
 }
-
 export default App
