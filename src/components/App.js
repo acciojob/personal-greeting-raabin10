fec989f50f825765f6e04641500b28866d9880e0
RAABIN SINGH  S A <112077212+raabin10@users.noreply.github.com>
2023-12-15 10:27:46 +0000

    h

--- src/components/App.js
+++ src/components/App.js
@@ -6,11 +6,13 @@ const App = () => {
   let [text, setText]  = useState('');
   return (
     <div>
+       <form>                  
       <p>Enter your name:</p>
        <input type="text"
        onChange={(e)=>setText(e.target.value)}
        value={text}/>
        {text.length > 0 ? <p>Hello {text}!</p>: null }
+       </form>
     </div>
   )
 }
