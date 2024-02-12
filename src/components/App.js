4ca2936b58a7bb2b8d5285069223e7666a6afc6a
RAABIN SINGH  S A <112077212+raabin10@users.noreply.github.com>
2024-02-12 18:13:10 +0000

    in

--- src/components/App.js
+++ src/components/App.js
@@ -3,17 +3,26 @@ import React ,{useState}from "react";
 import './../styles/App.css';
 
 const App = () => {
-  let [text, setText]  = useState('');
+ 
+  const [name, setName] = useState('');
+
+  const handleNameChange = (event) => {
+    setName(event.target.value);
+  };
+
   return (
     <div>
-       <form action="">                  
-      <p>Enter your name:</p>
-       <input type="text"
-       onChange={(e)=>setText(e.target.value)}
-       value={text}/>
-       </form>
-       <p>Hello {text}!</p>
+      <p>
+        Enter your name :
+      </p>
+      <input
+        type="text"
+        placeholder="Enter your name"
+        value={name}
+        onChange={handleNameChange}
+      />
+      {name && <p>Hello {name}!</p>}
     </div>
-  )
-}
+  );
+};
 export default App
